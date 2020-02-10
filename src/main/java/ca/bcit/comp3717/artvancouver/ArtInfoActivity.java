59842fa12e1d18cb7e2412ffde4bdcb4b7324e44
James Pike <james@jpike.ca>
2020-02-10 14:50:19 -0800

    Completed basic art info activity

---
+++ src/main/java/ca/bcit/comp3717/artvancouver/ArtInfoActivity.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp3717.artvancouver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArtInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art_info);
+    }
+}
